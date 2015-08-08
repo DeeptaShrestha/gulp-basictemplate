d212c9f93794b1cea473ec02cecd145d9d0159cf
Deepta <deepta@outlook.com>
2015-08-08 02:54:26 +0000

    First commit with the template files

---
+++ src/scripts/myscript.js
@@ -0,0 +1,10 @@
+    function changeImage() {
+        var image = document.getElementById('myImage');
+        if (image.src.match("bulbon")) {
+            image.src = "../images/pic_bulboff.gif";
+        } else {
+            image.src = "../images/pic_bulbon.gif";
+        }
+    }
+//comments
+  
